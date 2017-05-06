1614dd774b9be2079b1f9a80f1313003f2923484
Arthur A. Bergamaschi <arthurbergmz@gmail.com>
2017-05-06 19:33:53 -0300

    Fixing typings

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,20 @@
-// Type definitions for webapck-pwa-manifest 2.0.3
+// Type definitions for webpack-pwa-manifest 2.0.4
 // Project: https://github.com/arthurbergmz/webpack-pwa-manifest
-// Definitions by: Arthur A. Bergamaschi <www.arthurbergamaschi.com>
+// Definitions by: Arthur A. Bergamaschi <https://www.github.com/arthurbergmz>
 
-export as namespace webpackPwaManifest
+import { Plugin } from 'webpack';
 
 export = WebpackPwaManifest
 
-declare class WebpackPwaManifest {
+declare class WebpackPwaManifest extends Plugin {
     constructor(options: WebpackPwaManifest.ManifestOptions);
 }
 
 declare namespace WebpackPwaManifest {
-    export type Direction = 'ltr' | 'rtl' | 'auto';
-    export type Display = 'fullscreen' | 'standalone' | 'minimal-ui' | 'browser';
-    export type Orientation = 'any' | 'natural' | 'landscape' | 'landscape-primary' | 'landscape-secondary' | 'portrait' | 'portrait-primary' | 'portrait-secondary';
-    export interface ManifestOptions {
+    type Direction = 'ltr' | 'rtl' | 'auto';
+    type Display = 'fullscreen' | 'standalone' | 'minimal-ui' | 'browser';
+    type Orientation = 'any' | 'natural' | 'landscape' | 'landscape-primary' | 'landscape-secondary' | 'portrait' | 'portrait-primary' | 'portrait-secondary';
+    interface ManifestOptions {
         background_color?: string;
         description?: string;
         dir?: Direction;
@@ -31,12 +31,12 @@ declare namespace WebpackPwaManifest {
         start_url?: string;
         theme_color?: string;
     }
-    export interface RelatedApplications {
+    interface RelatedApplications {
         platform?: string;
         url: string;
         id?: string;
     }
-    export interface Icon {
+    interface Icon {
         src: string;
         size?: string | number;
         sizes?: number[];
