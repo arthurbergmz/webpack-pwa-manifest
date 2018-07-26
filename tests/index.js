a957b7293c7aa9324a6efd9d14d164c43d56487e
Doug Wade <dwade@indeed.com>
2018-07-26 11:00:26 -0700

    Add broken fingerprints: false test

--- tests/index.js
+++ tests/index.js
@@ -1,5 +1,5 @@
 const runTest = require('./runTest')
-const tests = ['basic', 'complex']
+const tests = ['basic', 'complex', 'fingerprints-false']
 
 console.log('Running tests...')
 runTest(tests.shift(), tests)
