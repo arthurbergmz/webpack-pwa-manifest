904060deba8996a7342edaf601aa1ab56fbf506e
Doug Wade <dwade@indeed.com>
2018-07-29 22:10:34 -0700

    [#65] Add test for output to directory

--- tests/index.js
+++ tests/index.js
@@ -1,5 +1,5 @@
 const runTest = require('./runTest')
-const tests = ['basic', 'complex', 'fingerprints-false']
+const tests = ['basic', 'complex', 'fingerprints-false', 'output-to-directory']
 
 console.log('Running tests...')
 runTest(tests.shift(), tests)
