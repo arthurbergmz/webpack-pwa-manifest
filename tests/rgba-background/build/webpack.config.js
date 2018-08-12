3f4b835ec5e03b5859ad706374ccc032ee661fdb
Doug Wade <dwade@indeed.com>
2018-08-12 11:02:07 -0700

    [#61] Support rgb and rgba colors

---
+++ tests/rgba-background/build/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifest = require('../../../dist')
+
+module.exports = {
+  entry: path.join(__dirname, '../app.js'),
+  output: {
+    path: path.join(__dirname, '../output'),
+    publicPath: '/',
+    filename: '[name].[hash].bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      minify: {
+        minifyCSS: true,
+        minifyJS: true,
+        collapseWhitespace: true,
+        collapseInlineTagWhitespace: true,
+        preserveLineBreaks: false,
+        removeAttributeQuotes: true,
+        removeComments: true
+      }
+    }),
+    new WebpackPwaManifest({
+      name: 'My Progressive Web App',
+      short_name: 'MyPWA',
+      description: 'My awesome Progressive Web App!',
+      background_color: 'rgba(123, 23, 12, 0.3)',
+      ios: true,
+      icons: [
+        {
+          src: path.resolve('./tests/icon.png'),
+          sizes: 512,
+          destination: 'icons'
+        },
+        {
+          src: path.resolve('./tests/icon.png'),
+          size: 1024,
+          destination: 'icons',
+          ios: true
+        },
+        {
+          src: path.resolve('./tests/icon.svg'),
+          sizes: 512,
+          destination: 'icons',
+          color: '#ffffff'
+        }
+      ]
+    })
+  ]
+}
